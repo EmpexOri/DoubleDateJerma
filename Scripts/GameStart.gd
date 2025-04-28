ea50bcfc15771de3703246d2f8617ae29dc6d9a5
EmpexOri <Oliver.rayward@icloud.com>
2025-04-28 22:13:30 +1000

    Finished

--- Scripts/GameStart.gd
+++ Scripts/GameStart.gd
@@ -8,6 +8,8 @@ extends Node
 var start_player : AudioStreamPlayer
 var delete_player : AudioStreamPlayer
 
+var skipped = false
+
 func _ready():
 	# Set up audio players
 	start_player = AudioStreamPlayer.new()
@@ -22,12 +24,18 @@ func _ready():
 	# Play the start sound
 	start_player.play()
 
-	# Set timers for the events
-	await get_tree().create_timer(10.0).timeout
-	_on_13_seconds()
+	# Start timers asynchronously
+	run_timers()
 
-	await get_tree().create_timer(6.0).timeout # 16s total (13 + 3)
-	_on_16_seconds()
+func run_timers() -> void:
+	# Using async function to wait on timers
+	await get_tree().create_timer(7.0).timeout
+	if not skipped:
+		_on_13_seconds()
+
+	await get_tree().create_timer(6.0).timeout # 16s total (10 + 6)
+	if not skipped:
+		_on_16_seconds()
 
 func _on_13_seconds():
 	if boot_node:
@@ -36,3 +44,8 @@ func _on_13_seconds():
 
 func _on_16_seconds():
 	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"): # default ESC key in Godot
+		skipped = true
+		get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
